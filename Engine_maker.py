747a72e6951ba6410a1bef7b297105ae65c3ef49
astro7628 <joehen012@gmail.com>
2026-04-26 09:34:23 -0400

    Refactor load_json and implement engine_make
    Refactor load_json function to use a global data path and improve input handling for engine_make function.

---
+++ Engine_maker.py
@@ -0,0 +1,28 @@
+import json 
+
+data_path = engine_stuff.json
+
+def load_json(): 
+    global data_path
+    path = data_path
+    with open(path, 'r') as file: # 3. Open file
+        data = json.load(file) # 4. Use json.load()
+    return data
+  
+load_json()
+
+def engine_make():
+  format = Input("format (ex. inline, V, W, rotary): ")
+  size = input("size: ")
+  if format == str(inline):
+    pass
+  elif format == str(v):
+    pass
+  elif format == str(vr):
+    pass
+  elif format == str(w):
+    pass
+  elif format == str(rotary):
+    pass
+  else:
+    pass
